310eb61c07f951fbbd18431ff2eaced43a486f33
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-20 18:13:53 +0100

    Build basic layout of Repository

--- app/src/main/java/se/stylianosgakis/devbytes/repository/VideosRepository.kt
+++ app/src/main/java/se/stylianosgakis/devbytes/repository/VideosRepository.kt
@@ -1,1 +1,18 @@
 package se.stylianosgakis.devbytes.repository
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import se.stylianosgakis.devbytes.database.VideosDatabase
+import se.stylianosgakis.devbytes.network.Network
+import se.stylianosgakis.devbytes.network.asDatabaseModel
+
+class VideosRepository(
+    private val database: VideosDatabase
+) {
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylistAsync().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+}
