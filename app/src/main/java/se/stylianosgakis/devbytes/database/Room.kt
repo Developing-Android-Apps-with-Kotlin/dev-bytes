0848979b93e410595ed69f8876f881e4216b7e5f
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-20 17:55:51 +0100

    Add VideoDao

--- app/src/main/java/se/stylianosgakis/devbytes/database/Room.kt
+++ app/src/main/java/se/stylianosgakis/devbytes/database/Room.kt
@@ -1,1 +1,16 @@
 package se.stylianosgakis.devbytes.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("SELECT * FROM databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg video: DatabaseVideo)
+}
