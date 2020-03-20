5c623c840da5978e394afe97f2c04d0d372437f3
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-20 18:04:40 +0100

    Add Room database creation

--- app/src/main/java/se/stylianosgakis/devbytes/database/Room.kt
+++ app/src/main/java/se/stylianosgakis/devbytes/database/Room.kt
@@ -1,10 +1,14 @@
 package se.stylianosgakis.devbytes.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Dao
 interface VideoDao {
@@ -13,4 +17,24 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg video: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideosDatabase::class.java,
+                "videos"
+            ).build()
+        }
+    }
+    return INSTANCE
 }
