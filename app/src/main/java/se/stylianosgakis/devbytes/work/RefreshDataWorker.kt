df82aab175e6c0a3ce7b293b72b19e169f893391
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-20 20:11:24 +0100

    Create background Worker that refreshes the videos

---
+++ app/src/main/java/se/stylianosgakis/devbytes/work/RefreshDataWorker.kt
@@ -0,0 +1,24 @@
+package se.stylianosgakis.devbytes.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import retrofit2.HttpException
+import se.stylianosgakis.devbytes.database.getDatabase
+import se.stylianosgakis.devbytes.repository.VideosRepository
+
+class RefreshDataWorker(
+    appContext: Context, params: WorkerParameters
+) : CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (exception: HttpException) {
+            Result.retry()
+        }
+    }
+}
